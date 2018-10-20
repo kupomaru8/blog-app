1394e58efe1ef2ea39712d7a76d1587b90ac75e8
朝木美帆 <asaki.miho@gmail.com>
2018-10-20 15:27:39 +0900

    Add function

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module BlogApp
   class Application < Rails::Application
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
